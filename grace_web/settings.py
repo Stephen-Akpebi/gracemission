802ac1e0da9c60ddf28ad6f2362e9347db0afacf
Stephen-Akpebi <www.nineclouds@gmail.com>
2023-02-10 19:41:27 +0000

    grace for heroku

--- grace_web/settings.py
+++ grace_web/settings.py
@@ -47,7 +47,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "grace"
+    "grace",
+    'cloudinary_storage',
+    'cloudinary',
 ]
 
 CRISPY_TEMPLATE_PACK = 'uni_form'
