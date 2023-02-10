3d2565d062d7be8641ff996c9b5b20f9a3b62a6e
Stephen-Akpebi <www.nineclouds@gmail.com>
2023-02-10 20:17:36 +0000

    grace for heroku

--- grace_web/settings.py
+++ grace_web/settings.py
@@ -10,14 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-from pathlib import Path
+
 from pathlib import Path
 import os
 import django_heroku
 import dj_database_url
 import sys
 from decouple import config
-import cloudinary
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
